7e8ad1d0458b20b359a2ab0c9c925ff2f488fc4a
Andrew Whiteside <andywhiteside@gmail.com>
2025-04-12 09:50:25 -0500

    feat: refactor DecisionTree component and restore binary tree generation logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,5 @@
-import DecisionTree, { generateBinaryTree } from './components/DecisionTree'
-
 function App() {
-  return (
-    <div className="p-6">
-      <DecisionTree data={generateBinaryTree(6)} />
-    </div>
-  )
+  return <div className="p-6"></div>
 }
 
 export default App
--- frontend/src/components/DecisionTree.tsx
+++ frontend/src/components/DecisionTree.tsx
@@ -8,6 +8,39 @@ export type TreeNode = {
   loc?: number // required by Nivo tree leaves
 }
 
+export default function DecisionTree({ data }: { data: TreeNode }) {
+  return (
+    <div style={{ height: 600 }}>
+      <ResponsiveTree
+        data={data}
+        identity="name"
+        nodeColor={(node) => (node.data as TreeNode).color}
+        activeNodeSize={24}
+        inactiveNodeSize={12}
+        fixNodeColorAtDepth={1}
+        linkThickness={6}
+        activeLinkThickness={8}
+        inactiveLinkThickness={2}
+        layout="left-to-right"
+        linkColor={{
+          from: 'target.color',
+          modifiers: [['opacity', 0.4]],
+        }}
+        margin={{ top: 90, right: 90, bottom: 90, left: 90 }}
+        motionConfig="stiff"
+        meshDetectionRadius={80}
+        label={(d) => (d.data as TreeNode).name}
+        linkTooltip={() => null}
+        onLinkMouseEnter={() => {}}
+        onLinkMouseMove={() => {}}
+        onLinkMouseLeave={() => {}}
+        onLinkClick={() => {}}
+        linkTooltipAnchor="top"
+      />
+    </div>
+  )
+}
+
 function getRandomColor(): string {
   const hue = Math.floor(Math.random() * 360)
   return `hsl(${hue}, 70%, 50%)`
@@ -37,34 +70,48 @@ export function generateBinaryTree(
   return node
 }
 
-export default function DecisionTree({ data }: { data: TreeNode }) {
-  return (
-    <div style={{ height: 600 }}>
-      <ResponsiveTree
-        data={data}
-        identity="name"
-        nodeColor={(node) => (node.data as TreeNode).color}
-        activeNodeSize={24}
-        inactiveNodeSize={12}
-        fixNodeColorAtDepth={2}
-        linkThickness={6}
-        activeLinkThickness={8}
-        inactiveLinkThickness={2}
-        linkColor={{
-          from: 'target.color',
-          modifiers: [['opacity', 0.4]],
-        }}
-        margin={{ top: 90, right: 90, bottom: 90, left: 90 }}
-        motionConfig="stiff"
-        meshDetectionRadius={80}
-        label={(d) => (d.data as TreeNode).name}
-        linkTooltip={() => null}
-        onLinkMouseEnter={() => {}}
-        onLinkMouseMove={() => {}}
-        onLinkMouseLeave={() => {}}
-        onLinkClick={() => {}}
-        linkTooltipAnchor="top"
-      />
-    </div>
-  )
+export const treeData = {
+  id: 'Node-1',
+  name: 'Node-1',
+  color: getRandomColor(),
+  children: [
+    {
+      id: 'NodeL-2',
+      name: 'NodeL-2',
+      color: getRandomColor(),
+      children: [
+        {
+          id: 'NodeLL-3',
+          name: 'NodeLL-3',
+          color: getRandomColor(),
+          loc: 0,
+        },
+        {
+          id: 'NodeLR-3',
+          name: 'NodeLR-3',
+          color: getRandomColor(),
+          loc: 0,
+        },
+      ],
+    },
+    {
+      id: 'NodeR-2',
+      name: 'NodeR-2',
+      color: getRandomColor(),
+      children: [
+        {
+          id: 'NodeRL-3',
+          name: 'NodeRL-3',
+          color: getRandomColor(),
+          loc: 0,
+        },
+        {
+          id: 'NodeRR-3',
+          name: 'NodeRR-3',
+          color: getRandomColor(),
+          loc: 0,
+        },
+      ],
+    },
+  ],
 }

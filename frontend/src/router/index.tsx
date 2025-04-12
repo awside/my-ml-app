a73e69a50533df9a4bd2d691cf8cd492f837e999
Andrew Whiteside <andywhiteside@gmail.com>
2025-04-12 11:09:49 -0500

    Reinvent components folder structure for future proofing

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,21 +1,21 @@
-import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/Home";
-import Projects from "../pages/Projects";
-import Tools from "../pages/Tools";
-import Resume from "../pages/Resume";
-import Contact from "../pages/Contact";
-import Layout from "../components/Layout";
+import { createBrowserRouter } from 'react-router-dom'
+import Home from '../pages/Home'
+import Projects from '../pages/Projects'
+import Tools from '../pages/Tools'
+import Resume from '../pages/Resume'
+import Contact from '../pages/Contact'
+import Layout from '../components/layout/Layout'
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Layout />,
     children: [
       { index: true, element: <Home /> },
-      { path: "projects", element: <Projects /> },
-      { path: "tools", element: <Tools /> },
-      { path: "resume", element: <Resume /> },
-      { path: "contact", element: <Contact /> },
+      { path: 'projects', element: <Projects /> },
+      { path: 'tools', element: <Tools /> },
+      { path: 'resume', element: <Resume /> },
+      { path: 'contact', element: <Contact /> },
     ],
   },
-]);
+])
